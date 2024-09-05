18cc7e7f41998fc31e2fd68b10daf8eec6a45d2b
pramuk2912 <22eg107b19@anurag.edu.in>
2024-09-05 14:12:54 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname,'app_server', 'views'));
+app.set('views', path.join(__dirname, 'app_server','views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
